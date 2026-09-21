dce3aef6e5c9219952624b44a73a1e096b70f6a3
andre <davidborckimmobiliengmbh@gmail.com>
2026-09-21 02:08:02 +0200

    chore: release 0.5.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.mediagram.android"
         versionCode = 1
-        versionName = "0.4.1"
+        versionName = "0.5.0"
     }
 
     buildTypes {
