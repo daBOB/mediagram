acae0b5b34c28485d82da56d70eb4dfc10fbc84b
andre <davidborckimmobiliengmbh@gmail.com>
2026-09-24 02:57:05 +0200

    chore(release): synchronize version 0.40.2 and package metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.mediagram.android"
         versionCode = 5
-        versionName = "0.40.0"
+        versionName = "0.40.2"
     }
 
     buildTypes {
